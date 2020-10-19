96fa38b25507a6c2efcd0ee13e93b4d8d6c41696
vdonoladev <victordonolaf@gmail.com>
2020-10-19 08:19:03 -0300

    Updates

--- password-manager.py
+++ password-manager.py
@@ -11,35 +11,42 @@ ADMIN_PASSWORD = "verystrongpassword"
 connect = input("Enter the administrative password:\n")
 
 while connect != ADMIN_PASSWORD:
-    print ('Incorrect password!\nTry again.')
+    print('Incorrect password!\nTry again.')
     connect = input("Enter the administrative password:\n")
     if connect == "q":
         break
 
 conn = sqlite3.connect('password_generator.db')
 
+
 def create_password(pass_key, service, admin_pass):
     return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8') + pass_key.encode('utf-8')).hexdigest()[:15]
+
 
 def get_hex_key(admin_pass, service):
     return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()
 
+
 def get_password(admin_pass, service):
     secret_key = get_hex_key(admin_pass, service)
-    cursor = conn.execute("SELECT * from KEYS WHERE PASS_KEY=" + '"' + secret_key + '"')
+    cursor = conn.execute(
+        "SELECT * from KEYS WHERE PASS_KEY=" + '"' + secret_key + '"')
 
     file_string = ""
     for row in cursor:
         file_string = row[0]
     return create_password(file_string, service, admin_pass)
 
+
 def add_password(service, admin_pass):
     secret_key = get_hex_key(admin_pass, service)
 
-    command = 'INSERT INTO KEYS (PASS_KEY) VALUES (%s);' %('"' + secret_key +'"')        
+    command = 'INSERT INTO KEYS (PASS_KEY) VALUES (%s);' % (
+        '"' + secret_key + '"')
     conn.execute(command)
     conn.commit()
     return create_password(secret_key, service, admin_pass)
+
 
 if connect == ADMIN_PASSWORD:
     try:
@@ -48,10 +55,9 @@ if connect == ADMIN_PASSWORD:
         print("Your database has been created!\nWhat password would you like to keep now?")
     except:
         print("You already have a database, what would you like to do?")
-    
-    
+
     while True:
-        print("\n"+ "*"*15)
+        print("\n" + "*"*15)
         print("Commands:")
         print("e = Exit the program")
         print("v = View password")
@@ -62,8 +68,11 @@ if connect == ADMIN_PASSWORD:
         if input_ == "e":
             break
         if input_ == "c":
-            service = input("Which service or website will this password be for?\n")
-            print("\n" + service.capitalize() + " password created:\n" + add_password(service, ADMIN_PASSWORD))
+            service = input(
+                "Which service or website will this password be for?\n")
+            print("\n" + service.capitalize() + " password created:\n" +
+                  add_password(service, ADMIN_PASSWORD))
         if input_ == "v":
             service = input("What is the name of the site/service?\n")
-            print("\n" + service.capitalize() + " password:\n"+get_password(ADMIN_PASSWORD, service))
+            print("\n" + service.capitalize() + " password:\n" +
+                  get_password(ADMIN_PASSWORD, service))
